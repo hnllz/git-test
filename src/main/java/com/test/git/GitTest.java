665912f7883a46db1e5a91b5d70f3294eec3b171
liliuzhen <306632427@qq.com>
2022-08-31 13:41:41 +0800

    push test1

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
